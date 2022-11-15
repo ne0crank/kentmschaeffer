b084c7d5ffedcb7239c286f00cf08bdfd260656b
kent schaeffer <ne0crank@icloud.com>
2022-11-15 06:43:28 -0700

    budget-mgr added as new project

---
+++ projects/budget-mgr/application/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <v-container>
+      <router-view/>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/styles";
+
+  body {
+    background: url('./assets/images/darren_family_photo.jpeg') no-repeat center center fixed;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
+</style>
---
+++ projects/budget-mgr/application/src/main.js
@@ -0,0 +1,8 @@
+import VueCookie from 'vue-cookie'
+import Vuetify from 'vuetify'
+import('../node_modules/vuetify/dist/vuetify.min.css')
+
+Vue.use(VueCookie)
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
