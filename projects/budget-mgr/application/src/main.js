4543c0d13d4c6a5d43d00b290bffc7805ec0499b
kent schaeffer <ne0crank@icloud.com>
2022-11-20 15:57:07 -0700

    merged several small projects to main repository

--- projects/budget-mgr/application/src/main.js
+++
@@ -1,8 +0,0 @@
-import VueCookie from 'vue-cookie'
-import Vuetify from 'vuetify'
-import('../node_modules/vuetify/dist/vuetify.min.css')
-
-Vue.use(VueCookie)
-Vue.use(Vuetify)
-
-Vue.config.productionTip = false
